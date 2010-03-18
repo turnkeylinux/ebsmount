d5c25a8738140a466d9148e7b5ca0942b79b8d12
Alon Swartz <alon@sterilesecurity.com>
2010-03-18 19:58:50 +0200

    created ebsmount-udev command to be called by udev trigger
    udev will pass environment variables:
        DEVNAME         (e.g., /dev/sdh)
        PHYSDEVPATH     (e.g., /devices/xen/vbd-2160)

---
+++ cmd_udev.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+"""EBS Mount - triggered by udev on EBS attach and detach
+
+Arguments:
+
+    action          action trigger (add | remove)
+
+Environment variables:
+
+    DEVNAME         (required: e.g., /dev/sdh)
+    PHYSDEVPATH     (required: e.g., /devices/xen/vbd-2160)
+"""
+
+import os
+import sys
+
+import ebsmount
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, "error: " + str(e)
+
+    print >> sys.stderr, "Syntax: %s <action>" % sys.argv[0]
+    print >> sys.stderr, __doc__.strip()
+    sys.exit(1)
+
+def fatal(s):
+    print >> sys.stderr, "error: " + str(s)
+    sys.exit(1)
+
+def main():
+    if not len(sys.argv) == 2:
+        usage()
+
+    action = sys.argv[1]
+    DEVNAME = os.getenv('DEVNAME', None)
+    PHYSDEVPATH = os.getenv('PHYSDEVPATH', None)
+
+    if not action in ('add', 'remove'):
+        usage('action must be one of: add, remove')
+
+    if not DEVNAME:
+        usage('DEVNAME is required')
+
+    if not PHYSDEVPATH:
+        usage('PHYSDEVPATH is required')
+
+    if not PHYSDEVPATH.startswith('/devices/xen/vbd-'):
+        usage('PHYSDEVPATH is not of the expected structure')
+
+    mountdir = os.path.join('/media/ebs', os.path.basename(PHYSDEVPATH))
+    func = getattr(ebsmount, 'ebsmount_' + action)
+    func(DEVNAME, mountdir)
+
+if __name__=="__main__":
+    main()
+
--- ebsmount.py
+++ ebsmount.py
@@ -1,51 +1,11 @@
-#!/usr/bin/python
-"""Example command template
 
-Options:
-  -q --quiet	don't print anything
-  -f=FOO	print FOO
-  --fatal       fatal error
+from os.path import *
 
-"""
-import sys
-import getopt
+def ebsmount_add(devname, mountdir):
+    """ebs device attached"""
+    pass
 
-def usage(e=None):
-    if e:
-        print >> sys.stderr, "error: " + str(e)
-
-    print >> sys.stderr, "Syntax: %s [args]" % sys.argv[0]
-    print >> sys.stderr, __doc__.strip()
-    sys.exit(1)
-
-def fatal(s):
-    print >> sys.stderr, "error: " + str(s)
-    sys.exit(1)
-
-def main():
-    try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'qf:h', ['fatal'])
-    except getopt.GetoptError, e:
-        usage(e)
-
-    opt_quiet = False
-    for opt, val in opts:
-        if opt == '-h':
-            usage()
-
-        if opt in ('-q', '--quiet'):
-            opt_quiet = True
-
-        elif opt == '-f':
-            print "printing foo: " + val
-
-        elif opt == '--fatal':
-            fatal("fatal condition")
-
-    if not opt_quiet:
-        print "printing args: " + `args`
-
-    
-if __name__=="__main__":
-    main()
+def ebsmount_remove(devname, mountdir):
+    """ebs device detached"""
+    pass
 
