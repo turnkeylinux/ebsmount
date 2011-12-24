4b436921e1bb52b1707679dd8f47edee316c2a8e
Russ Ferriday <russf@topia.com>
2011-12-24 06:04:14 +0000

    moved the hooks directory to the mount parent

--- ebsmount.py
+++ ebsmount.py
@@ -25,7 +25,7 @@ from utils import config, log, is_mounted, mount
 def ebsmount_add(devname, mountdir):
     """ebs device attached"""
 
-    log(devname, 'attached')
+    log(devname, 'added')
     matching_devices = []
     for device in udevdb.query():
         if device.name.startswith(basename(devname)):
@@ -35,8 +35,7 @@ def ebsmount_add(devname, mountdir):
         devpath = join('/dev', device.name)
         mountpath = join(mountdir, device.env.get('ID_FS_UUID', devpath[-1])[:6])
         mountoptions = ",".join(config.mountoptions.split())
-        hookspath = join(mountpath, ".ebsmount")
-
+        hookspath = join(split(mountpath)[0], '.ebsmount')
         filesystem = device.env.get('ID_FS_TYPE', None)
         if not filesystem:
             log(devname, "could not identify filesystem: %s" % devpath)
@@ -54,6 +53,7 @@ def ebsmount_add(devname, mountdir):
         mount(devpath, mountpath, mountoptions)
         log(devname, "mounted %s %s (%s)" % (devpath, mountpath, mountoptions))
 
+        log(devname, 'hookspath: %s' % hookspath)
         if exists(hookspath):
             hooks = os.listdir(hookspath)
             hooks.sort()
@@ -80,7 +80,7 @@ def ebsmount_add(devname, mountdir):
 def ebsmount_remove(devname, mountdir):
     """ebs device detached"""
 
-    log(devname, 'detached')
+    log(devname, 'removed')
     mounted = False
     for d in os.listdir(mountdir):
         path = join(mountdir, d)
